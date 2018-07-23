31c71eb907073c5def9c004b5825bb9db7b04068
Natalie <natalie.todd15@gmail.com>
2018-07-22 20:46:52 -0600

    console.log recognizes when typing into text boxes. W

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 const dinoData = './listings.json'
 //class component allows it to have state and methods
 class Form extends Component {
   state = {
-    //     title: '',
-    //     pay: '',
-    //     description: 
+    title: '',
+    pay: '',
+    description: ''
+  }
+  //essentially an event handler
+  handleChange = (event) => {
+    console.log('working')
   }
   render() {
     return (
       <form>
         <label htmlFor='title-box'>Title</label>
-        <input id='title-box' type='text' />
-        <label htmlFor='comp-box'>Compensation</label>
-        <input id='comp-box' type='text' />
+        <input id='title-box'
+          type='text'
+          value={this.state.title}
+          onChange={this.handleChange} />
+        <label htmlFor='pay-box'>Compensation</label>
+        <input id='pay-box'
+          type='text'
+          value={this.state.pay}
+          onChange={this.handleChange} />
         <label htmlFor='description-box'>Description</label>
-        <input id='description-box' type='text' />
+        <input id='description-box'
+          type='text'
+          value={this.state.description}
+          onChange={this.handleChange} />
         <button type='submit'>Submit</button>
       </form>
 
     )
   }
+
+
 }
-
-
 
 export default Form
