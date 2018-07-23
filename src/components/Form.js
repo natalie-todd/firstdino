4f810a8ce89cb95481534bc38f077462c27c7624
Natalie <natalie.todd15@gmail.com>
2018-07-23 14:36:10 -0600

    there was an extra data that should have been changed to a job EVERYTHING IS W

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const addJob = { title: '', pay: '', description: '', interested: [] };
-//class component allows it to have state and methods
+
 export default class Form extends React.Component {
   state = {
     statusMessage: '',
     job: { ...addJob }
   };
-  //essentially an event handler
+  
   handleChange = (event) => {
     const job = this.state.job;
     job[event.target.name] = event.target.value;
@@ -26,7 +26,7 @@ export default class Form extends React.Component {
       this.setState({ statusMessage: "Please Enter Salary Fields" });
       return;
     }
-    this.props.createJob(this.state.data);
+    this.props.createJob(this.state.job);
     this.setState({
       job: { ...addJob },
       statusMessage: 'Job Successfully Added!'
@@ -39,24 +39,23 @@ export default class Form extends React.Component {
         <label htmlFor='title-box'>Title</label>
         <input id='title-box'
           type='text'
-          value={this.state.title}
+          value={this.state.job.title}
           name='title'
           onChange={this.handleChange} />
         <label htmlFor='pay-box'>Compensation</label>
         <input id='pay-box'
           type='text'
-          value={this.state.pay}
+          value={this.state.job.pay}
           name='pay'
           onChange={this.handleChange} />
         <label htmlFor='description-box'>Description</label>
         <input id='description-box'
           type='text'
-          value={this.state.description}
+          value={this.state.job.description}
           name='description'
           onChange={this.handleChange} />
         <input type='submit' name='submit' value='Submit' />
         <div className='status-bar'>{this.state.statusMessage}</div>
-        {/* <button type='submit'>Submit</button> */}
       </form>
     )
   }
