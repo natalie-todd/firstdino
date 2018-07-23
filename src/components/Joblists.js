406400439b1bc28a9d245fa8c95d4d55c3a8f90a
Natalie <natalie.todd15@gmail.com>
2018-07-23 12:46:49 -0600

    changed syntax, still not recognizing id

--- src/components/Joblists.js
+++ src/components/Joblists.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
-// import ListItems from './ListItems'
-
-export default function List({ data }) {
-  if (!data || data.length < 1) return <h3>No Jobs Found</h3>;
-  console.log(data);
-  return (
-    <ul id="job-listings">
-      {
-        data.map(job => (
-          <li key={job.id}>
-            <h4>{job.title}</h4>
-            <small>{job.pay}</small>
-            <p>{job.description}</p>
-            <small>{`${job.interested.length} dinos are interested in this job`}</small>
-          </li>
-        ))}
-    </ul >
-  );
+export default function Joblists({ jobs }) {
+  if (!jobs || jobs.length < 1) return <h3>No Jobs Found</h3>;
+  console.log(jobs);
+  return <ul id="job-listings">
+    {jobs.map(job => <li key={job.id}>
+          <h4>{job.title}</h4>
+          <small>{job.pay}</small>
+          <p>{job.description}</p>
+          <small>{`${job.interested.length} dinos are interested in this job`}</small>
+        </li>)}
+  </ul >
 }
