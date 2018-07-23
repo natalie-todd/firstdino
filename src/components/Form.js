4aa2bf1cfb82de050c8b690960c0f5d64b0d5f3a
Natalie <natalie.todd15@gmail.com>
2018-07-22 22:17:35 -0600

    moved json, failing

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,25 +10,51 @@ class Form extends Component {
   }
   //essentially an event handler
   handleChange = (event) => {
-    console.log('working')
+    const key = event.target.name
+    const value = event.target.value
+
+    this.setState({
+      [key]: value
+    }, () => console.log(this.state))
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch(dinoData, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(response => response.json())
+    .then(newPost => {
+      console.log(newPost.created)
+    })
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor='title-box'>Title</label>
         <input id='title-box'
           type='text'
           value={this.state.title}
+          //name is the input that has just been changed
+          name='title'
           onChange={this.handleChange} />
         <label htmlFor='pay-box'>Compensation</label>
         <input id='pay-box'
           type='text'
           value={this.state.pay}
+          name='pay'
           onChange={this.handleChange} />
         <label htmlFor='description-box'>Description</label>
         <input id='description-box'
           type='text'
           value={this.state.description}
+          name='description'
           onChange={this.handleChange} />
         <button type='submit'>Submit</button>
       </form>
